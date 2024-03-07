ee370a9bb666fcd2fff1591a3e4ed861fd7cf1d0
Madala Vikas <vikasvicky2473@gmail.com>
2024-03-07 18:21:04 +0530

    first commit

---
+++ CNN.py
@@ -0,0 +1,76 @@
+from keras.datasets import mnist
+import keras
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.utils import plot_model
+# Model configuration
+batch_size = 250
+no_epochs = 2
+no_classes = 10
+validation_split = 0.2
+verbosity = 1
+# Load MNIST dataset
+(input_train, target_train), (input_test, target_test) = mnist.load_data()
+print(input_train)
+# Shape of the input sets
+input_train_shape = input_train.shape
+input_test_shape = input_test.shape
+# Keras layer input shape
+input_shape = (input_train_shape[1], input_train_shape[2], 1)
+# Reshape the training data to include channels
+input_train = input_train.reshape(input_train_shape[0], input_train_shape[1], input_train_shape[2], 1)
+input_test = input_test.reshape(input_test_shape[0], input_test_shape[1], input_test_shape[2], 1)
+# Parse numbers as floats
+input_train = input_train.astype('float32')
+input_test = input_test.astype('float32')
+# Normalize input data
+input_train = input_train / 255
+input_test = input_test / 255
+# Building a Model Without Batch Normalization
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(no_classes, activation='softmax'))
+model.summary()
+# Compile the model
+model.compile(loss=tensorflow.keras.losses.sparse_categorical_crossentropy,
+              optimizer=tensorflow.keras.optimizers.legacy.Adam(),
+              metrics=['accuracy'])
+
+# Fit data to model
+history = model.fit(input_train, target_train,
+                    batch_size=batch_size,
+                    epochs=no_epochs,
+                    verbose=verbosity,
+                    validation_split=validation_split)
+
+score = model.evaluate(input_test, target_test, verbose=0)
+print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['train', 'test'], loc='lower right')
+plt.title("Accuracy")
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss ')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+######################################################################################################
+# from keras.utils.vis_utils import plot_model
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
